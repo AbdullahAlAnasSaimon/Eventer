8fcd736efc98ffb05b60afd0f41e93824f2b16c5
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-17 20:32:19 +0600

    making header with clerk authentication

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/images/logo.svg";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src={logo} alt="Company-logo" />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3"></div>
+      </div>
+    </header>
+  );
 }
