cfbbbeeed3eb7963b43e46c911b2f863a5719b2c
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-17 21:14:16 +0600

    added shadcn sheet for small device navigation

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import menuIcon from "@/public/icons/menu.svg";
 
 export default function MobileNav() {
-  return <div>MobileNav</div>;
+  return (
+    <nav>
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image className="cursor-pointer" src={menuIcon} alt="menu icon" />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          {/* <Image /> */}
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 }
