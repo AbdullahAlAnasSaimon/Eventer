80401cf5595f6cdf5fed545a954ed4cb3eaefb6a
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-18 08:22:15 +0600

    added simple footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/images/logo.svg";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src={logo} alt="company-logo" width={128} height={38} />
+        </Link>
+
+        <p>&copy; 2024 Eventer. All rights reserved</p>
+      </div>
+    </footer>
+  );
 }
