33deeb7d925e3471e8d0069e8979d69340200c6b
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-19 17:21:31 +0600

    successfully connect with database

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rencz4l.mongodb.net/?retryWrites=true&w=majority`;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
@@ -9,14 +9,26 @@ export const connectToDatabase = async () => {
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: "eventer",
-      bufferCommands: false,
-    });
+  try {
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URI, {
+        dbName: "eventer",
+        bufferCommands: false,
+      });
 
-  cached.conn = await cached.promise;
+    cached.conn = await cached.promise;
 
-  return cached.conn;
+    // Check if the connection is successful
+    if (cached.conn) {
+      console.log("Database connection established successfully");
+    } else {
+      console.error("Failed to establish database connection");
+    }
+
+    return cached.conn;
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    throw error; // Re-throw the error to be handled elsewhere if needed
+  }
 };
