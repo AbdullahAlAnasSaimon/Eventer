f8c495efd881befd8752f2410620eacca5018669
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-19 16:29:52 +0600

    changed a small

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -9,10 +9,11 @@ export default function NavItems() {
   const pathname = usePathname();
   return (
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
-      {headerLinks?.map((link) => {
+      {headerLinks?.map((link, i) => {
         const isActive = pathname === link.route;
         return (
           <li
+            key={i}
             className={`${
               isActive && "text-primary-500"
             } flex-center p-medium-16 whitespace-normal`}
