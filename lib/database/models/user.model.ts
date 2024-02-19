510d1081520d832d9e2d91152bab17c7a7afb3f0
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-19 11:44:05 +0600

    created user schema

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,30 @@
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  photo: {
+    type: String,
+    require: true,
+  },
+});
