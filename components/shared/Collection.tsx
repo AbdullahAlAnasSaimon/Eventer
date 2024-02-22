3011511effa3e5fa099282e06817391ba58bae2b
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-22 21:04:30 +0600

    rendering the event section with event card

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
+import Card from "./Card";
 
 type CollectionProps = {
   data: IEvent[];
@@ -25,7 +26,20 @@ export default function Collection({
   return (
     <>
       {data.length > 0 ? (
-        <div></div>
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-full grid-cols-2 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+            {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organized";
+              const hidePrice = collectionType === "My_Tickets";
+
+              return (
+                <li key={event._id} className="flex justify-center">
+                  <Card />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       ) : (
         <div className="flex-center wrapper main-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
           <h3 className="p-bold-20">{emptyTitle}</h3>
