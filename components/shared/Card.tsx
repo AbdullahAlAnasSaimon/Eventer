1d05ca519ee673edcf9914e6873fbca9a439c592
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-23 15:16:29 +0600

    making the event card with data and styles

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,4 +1,5 @@
 import { IEvent } from "@/lib/database/models/event.model";
+import Link from "next/link";
 import React from "react";
 
 type CardProps = {
@@ -8,5 +9,24 @@ type CardProps = {
 };
 
 export default function Card({ event, hasOrderLink, hidePrice }: CardProps) {
-  return <div>Card</div>;
+  return (
+    <div className="group relative flex main-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+      <Link
+        href={`/events/${event._id}`}
+        style={{ backgroundImage: `url:(${event.imageUrl})` }}
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+      />
+
+      <Link
+        href={`/events/${event._id}`}
+        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
+      >
+        <div className="flex gap-2">
+          <span className="p-semibold-14 w-min rounded-lg bg-green-100 px-4 py-1 text-green-60">
+            {event.isFree ? "Free" : `${event.price}`}
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
 }
