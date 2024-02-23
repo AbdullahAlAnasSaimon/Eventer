fbcc133b6128186908d073d68450ed876e9a0b47
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-23 15:07:06 +0600

    pass and get props through component

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,5 +1,12 @@
+import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
 
-export default function Card() {
+type CardProps = {
+  event: IEvent;
+  hasOrderLink?: boolean;
+  hidePrice?: boolean;
+};
+
+export default function Card({ event, hasOrderLink, hidePrice }: CardProps) {
   return <div>Card</div>;
 }
