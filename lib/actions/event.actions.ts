5db45366fe20dabdee86d893c798b16753b6940e
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-25 22:36:10 +0600

    added update event server action

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -4,6 +4,7 @@ import {
   CreateEventParams,
   DeleteEventParams,
   GetAllEventsParams,
+  UpdateEventParams,
 } from "@/types";
 import { connectToDatabase } from "../database";
 import { handleError } from "../utils";
@@ -104,3 +105,25 @@ export const deleteEvent = async ({ eventId, path }: DeleteEventParams) => {
     handleError(error);
   }
 };
+
+export async function updateEvent({ userId, event, path }: UpdateEventParams) {
+  try {
+    await connectToDatabase();
+
+    const eventToUpdate = await Event.findById(event._id);
+    if (!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
+      throw new Error("Unauthorized or event not found");
+    }
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      event._id,
+      { ...event, category: event.categoryId },
+      { new: true }
+    );
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(updatedEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
