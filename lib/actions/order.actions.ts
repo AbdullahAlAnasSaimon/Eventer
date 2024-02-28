2240a032e4fd75f0b0dab3f8c7467e980abc5ca3
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-02-28 21:32:37 +0600

    create a new stripe sesstion

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -2,9 +2,36 @@
 
 import { CheckoutOrderParams } from "@/types";
 import { handleError } from "../utils";
+import Stripe from "stripe";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const price = order.isFree ? 0 : Number(order.price) * 100;
+
   try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: price,
+            product_data: {
+              name: order.eventTitle,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        eventId: order.eventId,
+        buyerId: order.buyerId,
+      },
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    });
   } catch (error) {
     handleError(error);
   }
