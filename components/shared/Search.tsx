42b4d92658e1062cec23495d573596d8b03469ff
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-03-02 23:11:28 +0600

    building the search bar

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import search from "@/public/icons/search.svg";
 
 export default function Search() {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-lg bg-grey-50 px-4 py-2">
+      <Image src={search} alt="search" width={24} height={24} />
+    </div>
+  );
 }
