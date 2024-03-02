058c9d5eacd55d9d571bdbedc7ddca4854acee08
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-03-02 23:22:04 +0600

    adding the functionality for search query

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,12 +1,36 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import search from "@/public/icons/search.svg";
 import { Input } from "../ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Search() {
   const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+      if (query) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: query,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+  }, [query]);
 
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-lg bg-grey-50 px-4 py-2">
