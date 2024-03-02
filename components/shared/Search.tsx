a4272c5cb85b6ac2fda357dc90300d2c1fd5e237
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-03-02 23:16:01 +0600

    added input field for getting the search querys

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import search from "@/public/icons/search.svg";
+import { Input } from "../ui/input";
 
 export default function Search() {
   const [query, setQuery] = useState("");
@@ -10,6 +11,12 @@ export default function Search() {
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-lg bg-grey-50 px-4 py-2">
       <Image src={search} alt="search" width={24} height={24} />
+      <Input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value)}
+        className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+      />
     </div>
   );
 }
