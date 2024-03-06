8136093f05fd88f81538aee8dea4e05d2e716877
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-03-06 21:46:05 +0600

    clear timed out and pass the placeholder to search input

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -7,7 +7,11 @@ import { Input } from "../ui/input";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function Search() {
+export default function Search({
+  placeholder = "Search title...",
+}: {
+  placeholder: string;
+}) {
   const [query, setQuery] = useState("");
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -30,14 +34,16 @@ export default function Search() {
 
       router.push(newUrl, { scroll: false });
     }, 300);
-  }, [query]);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searchParams, router]);
 
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-lg bg-grey-50 px-4 py-2">
       <Image src={search} alt="search" width={24} height={24} />
       <Input
         type="text"
-        placeholder="Search..."
+        placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
         className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
       />
