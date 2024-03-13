09308d42badae3c2a6430efeaa123efb2adc8533
abdullah <abdullahalanassaimon.swd@gmail.com>
2024-03-13 19:43:09 +0600

    added functionality for category filter component

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -5,8 +5,37 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function CategoryFilter() {
+  const [categories, setCategories] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+      if (categories) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: categories,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [categories, searchParams, router]);
+
   return (
     <Select>
       <SelectTrigger className="w-[180px]">
